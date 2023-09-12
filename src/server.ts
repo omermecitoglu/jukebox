e58e994b39f49a0c7980b94b25f8b42f1a2655fe
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-09-12 16:39:09 +0700

    add youtube downloader

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,11 @@
-import { createServer } from "node:http";
-import path from "path";
-import express from "express";
-import { Server } from "socket.io";
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  cors: (process.env.NODE_ENV === "production") ? undefined : {
-    origin: "http://localhost:3000",
-  },
-});
-
-const clientFolder = path.join(__dirname, "client");
-app.use(express.static(clientFolder));
+import io from "./core/socket";
+import { startDownload } from "./core/youtube";
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-});
-
-const serverPort = process.env.NODE_ENV === "production" ? 3000 : 3001;
-
-server.listen(serverPort, () => {
-  console.log("server running at http://localhost:" + serverPort);
+  socket.on("music:download:start", (input: string) => {
+    const videoId = startDownload(input);
+    if (videoId) {
+      socket.emit("music:download:subscription:add", videoId);
+    }
+  });
 });
