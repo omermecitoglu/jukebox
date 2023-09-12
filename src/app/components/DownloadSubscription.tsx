1a4d6e80c3a02f76fe875182c3f295d090ca3867
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-09-12 16:08:17 +0700

    add download subscription component

---
+++ src/app/components/DownloadSubscription.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { socket } from "../core/socket";
+
+type DownloadSubscriptionProps = {
+  downloadId: string,
+};
+
+const DownloadSubscription = ({
+  downloadId,
+}: DownloadSubscriptionProps) => {
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    const handleProgress = (p: number) => {
+      setPercentage(p);
+    };
+
+    socket.on("music:download:subscription:progress:" + downloadId, handleProgress);
+
+    return () => {
+      socket.off("music:download:subscription:progress:" + downloadId, handleProgress);
+    };
+  }, [downloadId]);
+
+  return <ProgressBar key={downloadId} animated now={percentage} />;
+};
+
+export default DownloadSubscription;
