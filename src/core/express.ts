b8e24ecd0b223221d55b7e17c516455b9a8bfbec
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-09-24 10:25:48 +0700

    add Accept-Ranges header to served music folder

--- src/core/express.ts
+++ src/core/express.ts
@@ -9,6 +9,10 @@ const clientFolder = path.join(process.cwd(), "../client/build");
 app.use(express.static(clientFolder));
 
 const downloadsFolder = path.join(process.cwd(), "downloads");
-app.use(express.static(downloadsFolder));
+app.use(express.static(downloadsFolder, {
+  setHeaders: (res) => {
+    res.set("Accept-Ranges", "none");
+  },
+}));
 
 export default server;
