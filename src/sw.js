64b9a76b3292b95c5990c27b2c348130c0609d49
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-10-06 11:10:11 +0700

    update service worker

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const appCacheName = "v1";
+const appCacheName = BUILD_ID;
 
 const protectedCache = [
   appCacheName,
@@ -7,33 +7,7 @@ const protectedCache = [
   "others",
 ];
 
-const appShellFiles = [
-  "manifest.json",
-  "android-icon-36x36.png",
-  "android-icon-48x48.png",
-  "android-icon-72x72.png",
-  "android-icon-96x96.png",
-  "android-icon-144x144.png",
-  "android-icon-192x192.png",
-  "apple-icon-57x57.png",
-  "apple-icon-60x60.png",
-  "apple-icon-72x72.png",
-  "apple-icon-76x76.png",
-  "apple-icon-114x114.png",
-  "apple-icon-120x120.png",
-  "apple-icon-144x144.png",
-  "apple-icon-152x152.png",
-  "apple-icon-180x180.png",
-  "apple-icon-precomposed.png",
-  "apple-icon.png",
-  "favicon-16x16.png",
-  "favicon-32x32.png",
-  "favicon-96x96.png",
-  "/favicon.ico",
-  "/", // index.html
-  "/index.js",
-  "/style.css",
-];
+const appShellFiles = STATIC_FILES;
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
