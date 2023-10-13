79b48ee683a0eee6a6bda632eec846f7520f2382
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-10-13 17:04:04 +0700

    fix update reload at initial launch issue

--- src/components/Updater.tsx
+++ src/components/Updater.tsx
@@ -11,16 +11,20 @@ const Updater = () => {
       try {
         if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
           const registration = await navigator.serviceWorker.register("/sw.js");
-          registration.addEventListener("updatefound", () => {
-            const newWorker = registration.installing;
-            if (!newWorker) return;
-            newWorker.addEventListener("statechange", () => {
-              if (newWorker.state === "installed") {
-                alert("Application has been updated!");
-                window.location.reload();
-              }
+          const cacheList = await caches.keys();
+          const versions = cacheList.filter(c => /^v\d+\.\d+\.\d+\.\w+$/.test(c));
+          if (versions.length) {
+            registration.addEventListener("updatefound", () => {
+              const newWorker = registration.installing;
+              if (!newWorker) return;
+              newWorker.addEventListener("statechange", () => {
+                if (newWorker.state === "installed") {
+                  alert("Application has been updated!");
+                  window.location.reload();
+                }
+              });
             });
-          });
+          }
         }
         if (isOnline) {
           await fetch("/api/socket");
